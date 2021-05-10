1358529b340dd45b99568a2c21ef01d9ce85edd8
thekristopl <kristopalka@gmail.com>
2021-05-10 15:19:59 +0200

    redesigning

--- src/com/ScrabbleSolver/PossibleWordsFinder.java
+++ src/com/ScrabbleSolver/PossibleWordsFinder.java
@@ -10,54 +10,69 @@ public class PossibleWordsFinder {
     private static char[] holder;
     private static int length;
 
-    private static ArrayList<Word> possibleWords;
 
     public static ArrayList<Word> getAll(StandardBoard boardObject, Holder holderObject) {
         char[][] board = boardObject.toCharArray();
         length = boardObject.getLength();
         holder = holderObject.toCharArray();
 
-        possibleWords = new ArrayList<>();
 
-        findAndAddVertical(board);
-        board = transpose(board);
-        findAndAddVertical(board);
+        ArrayList<Word> vertical = getVertical(board);
 
-        return possibleWords;
+        ArrayList<Word> verticalToRotate = getVertical(transpose(board));
+        ArrayList<Word> horizontal = rotateVerticalToHorizontal(verticalToRotate);
+
+
+        ArrayList<Word> allWords = new ArrayList<>();
+        allWords.addAll(vertical);
+        allWords.addAll(horizontal);
+        return allWords;
     }
 
-    private static void findAndAddVertical(char[][] board) {
+
+    private static ArrayList<Word> getVertical(char[][] board) {
+        ArrayList<Word> words = new ArrayList<>();
         for (int colNum = 0; colNum < length; colNum++) {
-            ArrayList<String> potentialWords = getPotentialFromColumn();
-            addWordsMeetingRules(potentialWords);
+            ArrayList<String> potentialWords = getPotentialWordsFromColumn(board[colNum]);
+            ArrayList<Word> correctWords = selectCorrectWords(potentialWords);
 
+            words.addAll(correctWords);
         }
+        return words;
     }
 
 
-    private static ArrayList<String> getPotentialFromColumn() {
+    private static ArrayList<String> getPotentialWordsFromColumn(char[] column) {
         ArrayList<String> potentialWords = new ArrayList<>();
         //todo słowa które da się ułożyć z liter które są
 
         return potentialWords;
     }
 
-    private static void addWordsMeetingRules(ArrayList<String> potentialWords) {
+    private static ArrayList<Word> selectCorrectWords(ArrayList<String> potentialWords) {
+        ArrayList<Word> correctWords = new ArrayList<>();
         //todo wybierz te które: realnie da się ułożyć, nie kolidują z niczym
         //todo dodaj do globalnej possibleWords
+
+        return correctWords;
     }
+
 
     public static char[][] transpose(char[][] board) {
         char[][] transposedBoard = new char[length][length];
-
         for (int x = 0; x < length; x++) {
             for (int y = 0; y < length; y++) {
                 transposedBoard[x][y] = board[y][x];
             }
         }
-
         return transposedBoard;
     }
 
+
+    private static ArrayList<Word> rotateVerticalToHorizontal(ArrayList<Word> verticalToRotate) {
+        ArrayList<Word> rotated = new ArrayList<>();
+        //todo obracanie słów
+        return rotated;
+    }
 
 }
