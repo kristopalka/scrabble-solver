585c268dcc4af4103c3495e0455d5bef3dcf1dff
Krzysztof Pałka <krzysztof.palka@stazysta.comarch.pl>
2021-07-27 15:31:40 +0200

    Solver finish

--- src/main/java/com/scrabblewinner/solver/Solver.java
+++ src/main/java/com/scrabblewinner/solver/Solver.java
@@ -3,17 +3,17 @@ package com.scrabblewinner.solver;
 import com.scrabblewinner.scrabble.board.StandardBoard;
 import com.scrabblewinner.scrabble.holder.StandardHolder;
 import com.scrabblewinner.scrabble.board.components.Word;
+import com.scrabblewinner.solver.pointscalculator.PointsCalculatorForStandardBoard;
 import com.scrabblewinner.solver.wordsfinder.WordsFinder;
 
 import java.util.ArrayList;
 
 public class Solver
 {
-    public static void getBestWords(StandardBoard board, StandardHolder holder, int numberOfWordsToGet)
+    public static Word getBestWord(StandardBoard board, StandardHolder holder)
     {
         ArrayList<Word> possibleWords = WordsFinder.getAllPossibleWords(board, holder);
-        //todo calculate points for each word
-        //todo sort and return words with best score
+        return PointsCalculatorForStandardBoard.getBest(possibleWords);
     }
 
 }
